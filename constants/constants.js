e4404066d2911b24e5d47832ef629c06093d25ec
Nikunj Goel <72417259+nikunj-oss@users.noreply.github.com>
2024-10-11 13:51:58 +0530

    update cors

--- constants/constants.js
+++ constants/constants.js
@@ -1,5 +1,5 @@
 export const corsOptions={
-    origin:["http://localhost:5173","http://localhost:5173/api/v1/user"],
+    origin:["http://localhost:5173","http://localhost:5173/api/v1/user","https://snackit-client.vercel.app"],
     methods:["GET","POST","PUT","PATCH","DELETE"],
     credentials:true
 }
